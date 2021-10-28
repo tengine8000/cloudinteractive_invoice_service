1959050af556af62ead104b035bf98e36d721856
tengine8000 <torty.emmanuel@gmail.com>
2021-10-28 04:04:21 +0100

    Add AuthController and routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Roles;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        $user->save();
+
+        return response()->json(['user' => $user], 200);
+    }
+    
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $user->createToken('login')->plainTextToken], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        // Revoke all tokens...
+        $user->tokens()->delete();
+        
+        return response()->json([
+            'message' => 'Successfully logged out!'], 200);
+    }
+}
